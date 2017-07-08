da5eb13b90bf3489a18b74436f8fd044f43d6e5a
Kevin Lee <leekevin@berkeley.edu>
2017-07-08 01:35:17 +0000

    Complete V4

--- app/controllers/v4_controller.rb
+++ app/controllers/v4_controller.rb
@@ -1,7 +1,16 @@
+require 'twitter'
+
 class V4Controller < ApplicationController
   def home
   end
   def twitter
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "jPMFoP5lTYFxnJQgrhejHVFnR"
+      config.consumer_secret     = "bntUMRFIOpL0pnm6gjF3hqNNBypPUZfXn0kZ3wsM0cl5TtMDaU"
+      config.access_token        = "883417707013472256-pGxa9RpFZVfQMDvWACbgE1LiOg8hJyT"
+      config.access_token_secret = "dsC6U7v1TZd5HJnAAKTxLJzUNfDueHVM7eTnFQHo25p4i"
+    end
+    @tweets = client.search("to:YosemiteNPS traffic", result_type: "recent").take(3).collect
   end
   def twilio
   end
